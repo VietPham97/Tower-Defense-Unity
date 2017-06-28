cf18f2def0340362144e6c15fbf8c490da0bac7b
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-06-28 17:14:13 -0400

    Move the enemy from the starting point to the first waypoint with a speed of 10.

---
+++ Tower Defense Brackeys/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+    public float speed = 10f;
+
+    Transform target;
+    int wavepointIndex;
+
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+    }
+}
