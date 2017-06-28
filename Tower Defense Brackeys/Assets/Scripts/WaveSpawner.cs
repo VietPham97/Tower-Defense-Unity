143f083e618404b807df7e692c2d1b4ac4ef5504
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-06-28 17:44:49 -0400

    Use coroutine to spawn enemy not on top of each other.

---
+++ Tower Defense Brackeys/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour 
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    float countdown = 2f;
+
+    int waveIndex;  // default initialize to 0
+
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine("SpawnWave");
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);  // TODO get rid of hard coding
+        }
+    }
+
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
