c7d0bcd768fe3fe969aa1837781c0550c2ada4dd
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-06-29 15:13:19 -0400

    Create a singleton of BuildManager class to control all turrets.

---
+++ Tower Defense Brackeys/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour 
+{
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+	public GameObject TurretToBuild { get; private set; }
+    public GameObject standardTurretPrefab;
+
+    private void Start()
+    {
+        TurretToBuild = standardTurretPrefab;
+    }
+
+}
