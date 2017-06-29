ae9594f93ae0a69fc6031f7201aff21a1f0e5625
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-06-29 14:02:54 -0400

    Call Shoot method every 0.5 second after lock at target.

--- Tower Defense Brackeys/Assets/Scripts/Turret.cs
+++ Tower Defense Brackeys/Assets/Scripts/Turret.cs
@@ -10,6 +10,9 @@ public class Turret : MonoBehaviour
     public Transform partToRotate;
     public float turnSpeed = 10f;
 
+    public float fireRate = 1f;
+    float fireCountdown;
+
 
     private void Start()
     {
@@ -55,6 +58,20 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+    }
+
+
+    private void Shoot()
+    {
+        Debug.Log("SHOOT!");
     }
 
 
