9cbe9dd9a8cf5ec8a80cbd055692278a2a2cae8d
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-06-29 13:55:41 -0400

    Smoothly rotate the turret to look at target.

--- Tower Defense Brackeys/Assets/Scripts/Turret.cs
+++ Tower Defense Brackeys/Assets/Scripts/Turret.cs
@@ -8,6 +8,7 @@ public class Turret : MonoBehaviour
     const string enemyTag = "Enemy";
 
     public Transform partToRotate;
+    public float turnSpeed = 10f;
 
 
     private void Start()
@@ -49,9 +50,10 @@ public class Turret : MonoBehaviour
     {
         if (target == null) return;
 
+        // Target lock on
         Vector3 dir = target.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(dir);
-        Vector3 rotation = lookRotation.eulerAngles;
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
     }
 
