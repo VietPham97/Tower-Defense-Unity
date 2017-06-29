e8d89a11e08fef6e63183eadcab5b2d6a830b09c
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-06-29 13:30:26 -0400

    Visualize the turret's range on scene.

---
+++ Tower Defense Brackeys/Assets/Scripts/Turret.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour 
+{
+    public Transform target;
+    public float range = 15f;
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
