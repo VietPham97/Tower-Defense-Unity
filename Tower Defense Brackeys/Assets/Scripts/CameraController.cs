7dc1f6f1a6861498c316fc87fa961f89e113bbe9
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-06-29 15:29:13 -0400

    Create a simple RTS camera movement by move mouse to the edge of the screen.

---
+++ Tower Defense Brackeys/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+
+		if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= panBorderThickness)
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - panBorderThickness)
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= panBorderThickness)
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        
+    }
+}
