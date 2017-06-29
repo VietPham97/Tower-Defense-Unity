b0e9df504581e029687d96d6387c2106d00dac8e
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-06-29 14:53:42 -0400

    Change the node colour when mouse cursor hover onto it.

---
+++ Tower Defense Brackeys/Assets/Scripts/Node.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour 
+{
+    public Color hoverColor;
+
+    Renderer rend;
+    Color startColor;
+
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>(); // Always put GetComponent method in Start function for optimization
+        startColor = rend.material.color;
+    }
+
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
