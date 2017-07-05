e7768dea611fffead690a59a5c54ed346596c958
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-07-04 20:17:21 -0400

    Set up Laser Beamer turret.

--- Tower Defense Brackeys/Assets/Scripts/Shop.cs
+++ Tower Defense Brackeys/Assets/Scripts/Shop.cs
@@ -4,6 +4,7 @@ public class Shop : MonoBehaviour
 {
     public TurretBlueprint standardTurret;
     public TurretBlueprint missileLauncher;
+    public TurretBlueprint laserBeamer;
 
     BuildManager buildManager;
 
@@ -15,13 +16,19 @@ public class Shop : MonoBehaviour
 
     public void SelectStandardTurret()
     {
-        Debug.Log("Standard Turret Selected");
+        //Debug.Log("Standard Turret Selected");
         buildManager.TurretToBuild = standardTurret;
     }
 
 	public void SelectMissileLauncher()
 	{
-		Debug.Log("Missile Launcher Selected");
+		//Debug.Log("Missile Launcher Selected");
         buildManager.TurretToBuild = missileLauncher;
+	}
+
+	public void SelectLaserBeamer()
+	{
+		//Debug.Log("Missile Launcher Selected");
+        buildManager.TurretToBuild = laserBeamer;
 	}
 }
