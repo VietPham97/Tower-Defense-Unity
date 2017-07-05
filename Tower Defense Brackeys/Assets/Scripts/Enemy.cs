2fdbbfc825579103ea8e59f82b1b88de56727f8f
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-07-05 14:41:34 -0400

    Move all enemy movement control to a separated class.

--- Tower Defense Brackeys/Assets/Scripts/Enemy.cs
+++ Tower Defense Brackeys/Assets/Scripts/Enemy.cs
@@ -7,15 +7,7 @@ public class Enemy : MonoBehaviour
 
     public int moneyReward = 50;
 
-    Transform target;
-    int waypointIndex;
-
-    public GameObject deathEffect;
-
-    private void Start()
-    {
-        target = Waypoints.points[0];
-    }
+	public GameObject deathEffect;
 
     public void TakeDamage(float amount)
     {
@@ -34,35 +26,5 @@ public class Enemy : MonoBehaviour
         var effect = Instantiate(deathEffect, transform.position, Quaternion.identity) as GameObject;
         Destroy(effect, 5f);
         Destroy(gameObject);
-    }
-
-    private void Update()
-    {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-
-        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
-        {
-            GetNextWaypoint();
-        }
-    }
-
-
-    private void GetNextWaypoint()
-    {
-        if (waypointIndex >= Waypoints.points.Length - 1)
-        {
-            EndPath();
-            return;
-        }
-
-        waypointIndex++;
-        target = Waypoints.points[waypointIndex];
-    }
-
-    void EndPath()
-    {
-        PlayerStats.Lives--;
-		Destroy(gameObject);
     }
 }
---
+++ Tower Defense Brackeys/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyMovement : MonoBehaviour 
+{
+	Transform target;
+	int waypointIndex;
+
+    Enemy enemy;
+
+	private void Start()
+	{
+        enemy = GetComponent<Enemy>();
+		target = Waypoints.points[0];
+	}
+
+	private void Update()
+	{
+		Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+		{
+			GetNextWaypoint();
+		}
+	}
+
+
+	private void GetNextWaypoint()
+	{
+		if (waypointIndex >= Waypoints.points.Length - 1)
+		{
+			EndPath();
+			return;
+		}
+
+		waypointIndex++;
+		target = Waypoints.points[waypointIndex];
+	}
+
+	void EndPath()
+	{
+		PlayerStats.Lives--;
+		Destroy(gameObject);
+	}
+}
