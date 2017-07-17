2b8611629de0b66748bc98566f099ed602680366
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-07-16 23:09:50 -0400

    Create a Menu scene with a Play button to start game and a Quit button to quit game.

---
+++ Tower Defense Brackeys/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour 
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting...");
+        Application.Quit();
+    }
+}
