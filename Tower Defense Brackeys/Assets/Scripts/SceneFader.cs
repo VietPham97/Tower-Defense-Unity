50c4f8a37ac4dcf158b557cd829b386304b13e27
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-07-20 15:05:43 -0400

    Animate fade-in when game is started.

---
+++ Tower Defense Brackeys/Assets/Scripts/SceneFader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneFader : MonoBehaviour 
+{
+    public Image img;
+    public AnimationCurve curve;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 1f;
+
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0;
+        }
+    }
+}
